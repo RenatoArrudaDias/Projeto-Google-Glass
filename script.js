e523c62d13b0b1a546318a7b35de006483b8eb99
Renato <radrockblack@gmail.com>
2022-01-29 15:57:27 -0300

    projeto-glass-2022

--- script.js
+++
@@ -1,9 +0,0 @@
-/*Qual o resultado esperado após a execução do código abaixo?*/
-
-function validaNumero(num){
-    if (!num) return 'Envie um número válido!';
-  
-    return num;
-}
-
-validaNumero();
